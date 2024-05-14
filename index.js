c7290ebd6095dd4fd1c09934764463bb6cd72d92
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-15 04:20:59 +0600

    in backend site delete used

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -42,6 +42,14 @@ async function run() {
       const newPost = req.body;
       console.log(newPost);
       const result = await postCollection.insertOne(newPost);
+      res.send(result)
+    })
+
+    // delete query
+    app.delete('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await postCollection.deleteOne(query)
       res.send(result)
     })
 
